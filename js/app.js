d595555ffad82024a5c6f13cb34b2ea40a7b89eb
Eric Adlam <eadlam@gmail.com>
2013-12-11 12:55:54 +0100

    adding existing hoh website

---
+++ js/app.js
@@ -0,0 +1,23 @@
+'use strict';
+
+// Declare app level module which depends on filters, and services
+angular.module('hoh', ['hoh.filters', 
+                        'hoh.services', 
+                        'hoh.directives',
+                        'hoh.controllers']).
+  config(['$routeProvider', function($routeProvider) {
+
+    $routeProvider.when('/:lang/home', {templateUrl: 'partials/home.html',
+                                  controller: 'Main'});
+
+    $routeProvider.when('/:lang/about', {templateUrl: 'partials/about.html',
+                                  controller: 'Main'});
+
+    $routeProvider.when('/:lang/contact', {templateUrl: 'partials/contact.html',
+                                  controller: 'Main'});
+
+    $routeProvider.when('/:lang/donate', {templateUrl: 'partials/donate.html',
+                                  controller: 'Main'});
+
+    $routeProvider.otherwise({redirectTo: '/en/home'});
+  }]);
